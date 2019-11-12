0d7ca5fbdfeecd37732ec98721a7d26d10e44574
yaofei zheng <zyf26256@gmail.com>
2019-11-12 15:30:04 +0800

    add CANOpen position mode script

---
+++ CANOpen_scripts/position.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cansend can0 000#0102
+cansend can0 602#2F60600001
+cansend can0 602#23836000A0860100
+cansend can0 602#23846000A0860100
+cansend can0 602#2B4060000600
+cansend can0 602#2B4060000700
+cansend can0 602#2B4060002F00
+cansend can0 602#237A6000A0860100
+cansend can0 602#23816000404B4C00
+cansend can0 602#40646000
+cansend can0 602#2B4060003F00
+cansend can0 602#2B4060002F00
+sleep 5
+cansend can0 602#40646000
